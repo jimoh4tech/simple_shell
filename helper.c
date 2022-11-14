d97803169b721c76c8cd317ec555823ab329d0f2
root <root@ABU-ABDILLAH.localdomain>
2022-11-14 20:14:42 +0100

    Added helper functions

---
+++ helper.c
@@ -0,0 +1,136 @@
+#include "shell.h"
+
+/**
+ * stripe_newline - removes newline character
+ * @s:pointer to the string
+ * 
+ * Return: pointer to the stripped string
+ */
+
+char *stripe_newline(char *s)
+{
+	int i;
+	for (i = 0; s[i] != '\0'; i++)
+		if (s[i] == '\n')
+			s[i] = '\0';
+	return (s);
+}
+
+
+/**
+ * trim  - removes all spaces characters in a string
+ * @s:pointer to the string
+ *
+ * Return: the trimmed string
+ */
+
+char *trim(char *s)
+{
+	size_t l = _strlen(s);
+	while (isspace(s[l - 1]))
+	       --l;
+	while (*s && isspace(*s))
+	{
+		++s;
+		--l;
+	}
+	       return (strndup(s, l));
+		 
+}
+
+
+/**
+ * _getline - reads a new line
+ * @lineptr: pointer to the string pointer
+ * @n: pointer to the size
+ * @stream: pointer inputs streams
+ *
+ * Return: length of the string
+ */
+
+size_t _getline(char **lineptr, size_t *n, FILE *stream)
+{
+	static char line[256];
+	char *ptr;
+	size_t len;
+
+	if (lineptr == NULL || ferror(stream) || feof(stream))
+		return (-1);
+
+	fgets(line, 256, stream);
+
+	ptr = strchr(line, '\n');
+	if (ptr)
+		*ptr = '\0';
+
+	len = _strlen(line);
+	if ((len + 1) < 256)
+	{
+		ptr = realloc(*lineptr, 256);
+		if (ptr == NULL)
+			return (-1);
+		*lineptr = ptr;
+		*n = 256;
+	}
+	strcpy(*lineptr, line);
+	return (len);
+}
+
+
+/**
+ * before_delim - gets the value of string before a delimiter
+ * @s: pointer to the string
+ * @c: the delimeter to stop at
+ *
+ * Return: pointer to the string
+ */
+
+char *before_delim(char *s, char c)
+{
+	unsigned int i = 0;
+	char *value;
+
+	value = malloc(sizeof(char) * _strlen(s));
+	if (!value)
+		exit(1);
+	
+	while (s[i] != '\0' && s[i] != c)
+	{
+		value[i] = s[i];
+		i++;
+	}
+	value[i] = '\0';
+
+	return (value);
+}
+
+/**
+ * after_delim - gets the pointer to the string after delimiter
+ * @s: pointer to the string
+ * @c: the delimiter character
+ *
+ * Return: pointer to the string
+ */
+
+char *after_delim(char *s, char c)
+{
+  unsigned int i = 0, j = 0;
+	char *value;
+
+	value = malloc(sizeof(char) * _strlen(s));
+	if (!value)
+		exit(1);
+
+	while(s[i] != '\0' && s[i] != c)
+		i++;
+
+	while (s[i] != '\0')
+	{
+		++i;
+		value[j] = s[i];
+		j++;
+	}
+	value[j] = '\0';
+
+	return (value);
+}
---
+++ more_helper.c
@@ -0,0 +1,139 @@
+#include "shell.h"
+
+/**
+ * execute - runs commands
+ * @argv: args vector
+ * @filename: name of the file
+ */
+
+void execute(char **argv, char *filename)
+{
+	extern char **environ;
+	pid_t pid;
+	int status = 0;
+
+	pid = fork();
+	if (pid == -1)
+	{
+  		perror(filename);
+		exit(1);
+	}
+
+	if (pid == 0)
+	{
+		if (execve(argv[0], argv, environ) == -1)
+		{
+			perror(filename);
+			exit(1);
+		}
+	}
+	else
+	{
+	 	wait(&status);
+	}
+		  
+}
+
+/**
+ * parser - helps to parse commands
+ * @argv: pointer to the string
+ * @filename: filename
+ */
+
+void parser(char **argv, char *filename)
+{
+	if (_strcmp(argv[0], "ls"))
+	{
+		argv[0] = "/bin/ls";
+		execute(argv, filename);
+	}
+	else if (_strcmp(argv[0], "exit"))
+		__exit(argv[1]);
+	else if (_strcmp(argv[0], "env"))
+		printenv();
+	else if (_strcmp(argv[0], "cd"))
+		_chdir(argv[1]);
+	else if (_strcmp(argv[0], "setenv"))
+	  _setenv(argv[1], argv[2], 0);
+	else
+	{
+		execute(argv, filename);
+	}
+}
+
+/**
+ * tokenize - splits string into array of strings
+ * using the appropriate delimiter
+ * @s: pointer to the string
+ * @delim: the delimeter
+ *
+ * Return: pointer to the array token
+ */
+
+char **tokenize(char *s, char delim)
+{
+	int cur = 0, i = 0, idx = 0;
+	char **tokens, *token;
+
+	tokens = malloc(sizeof(char *) * 10);
+	if (!tokens)
+		exit(1);
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	  _putchar(s[i]);
+		while (s[i] != delim && s[i] != '\0')
+		{
+			token[cur] = s[i];
+			cur++;
+			i++;
+		}
+		token[cur] = '\0';
+		cur = 0;
+		tokens[idx] = token;
+		idx++;
+		token = NULL;
+	}
+	tokens[idx] = NULL;
+	return (tokens);
+}
+
+/**
+ * free_grid - frees allocated memory location
+ * @av: poimter to the array to be freed
+ */
+
+void free_grid(char **av)
+{
+	int i = 0;
+
+	while (av[i] != NULL)
+	{
+		free(av[i]);
+		i++;
+	}
+	free(av);
+}
+
+/**
+ * first_token - returns the string before space
+ * @s: point to the commads string
+ *
+ * Return: pointer to the string before space
+ */
+
+char *first_token(char *s)
+{
+	unsigned int i = 0;
+	char *token;
+
+	token  = malloc(sizeof(char) * 10);
+	if (!token)
+		exit(1);
+
+	for (i = 0; s[i] != '\0' && s[i] != ' '; i++)
+		token[i] = s[i];
+
+	token[i] = '\0';
+	return (token);
+}
